090eb5db83ef1353ba14fa61761c88b3ebfce81b
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-15 22:39:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
-  elsif phrase.downcase?
+  elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP SONNY!"
   end
 end
