258538598d3afaf94208a93ae279a8feefdbd855
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-15 22:43:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
     
   elsif phrase != phrase.upcase
-    return "HUH?! SPEAK UP SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
